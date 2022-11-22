43d502a21d0630ba4f7d69bc49d1bc329ed5d35a
blowfishfugu <coolm12345@hotmail.de>
2022-11-22 23:54:33 +0100

    assets-path, relative to debug and release-directory

---
+++ MoveSemantic.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+TEST(move_semantic, move1)
+{
+
+}
---
+++ UseFilesystem.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+
+#include <filesystem>
+#include <string>
+#include <iostream>
+
+#include <Windows.h>
+
+namespace fs = std::filesystem;
+
+TEST(UseFileSystem, check_relative)
+{
+	std::wstring strFile;
+	strFile.resize(MAX_PATH, '\0');
+	GetModuleFileName(NULL, strFile.data(), MAX_PATH);
+
+	fs::path exePath(strFile);
+	fs::path parentPath(exePath.parent_path());
+	fs::path assetsPath = parentPath.parent_path() / "assets";
+
+	std::cout << exePath.string() << "\n"
+		<< parentPath.string() << "\n"
+		<< assetsPath.string() << "\n";
+	
+	if (!fs::exists(assetsPath))
+	{
+		fs::create_directory(assetsPath);
+	}
+	ASSERT_TRUE(fs::exists(assetsPath));
+}
