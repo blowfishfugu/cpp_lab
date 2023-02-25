4eb99f5871f91b477245d507141d956116d1d8d7
blowfishfugu <coolm12345@hotmail.de>
2023-02-25 18:18:00 +0100

    Hausnummern haben auch Buchstaben a,b,c,...

--- sequential_read/GeoLocation.h
+++ sequential_read/GeoLocation.h
@@ -8,11 +8,11 @@ struct GeoLoc
 	using I = unsigned int;
 	using D = double;
 	//Berlin;Aachener Straße;1;10713;Charlottenburg-Wilmersdorf;Wilmersdorf;Wilmersdorf;52.482187140;13.318354210
-	std::tuple<S, S, I, I, S, S, S, D, D> raw_data{};
+	std::tuple<S, S, S, I, S, S, S, D, D> raw_data{};
 
 	S& City() { return std::get<0>(raw_data); }
 	S& Street() { return std::get<1>(raw_data); }
-	I& House() { return std::get<2>(raw_data); }
+	S& House() { return std::get<2>(raw_data); }
 	I& Zip() { return std::get<3>(raw_data); }
 	S& UrbanName() { return std::get<4>(raw_data); }
 	S& OldName() { return std::get<5>(raw_data); }
