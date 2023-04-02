3ecde28e5d9484225e4a90d41b4ec6778b3a2bce
blowfishfugu <coolm12345@hotmail.de>
2023-04-02 16:05:08 +0200

    fs::create_directories+fs::remove_all auf ../assets/sub/subsub

--- UseFilesystem.cpp
+++ UseFilesystem.cpp
@@ -28,3 +28,23 @@ TEST(UseFileSystem, check_relative)
 	}
 	ASSERT_TRUE(fs::exists(assetsPath));
 }
+
+TEST(UseFileSystem, directories)
+{
+	std::string strFile;
+	strFile.resize(MAX_PATH, '\0');
+	GetModuleFileName(NULL, strFile.data(), MAX_PATH);
+
+	fs::path exePath(strFile);
+	fs::path parentPath(exePath.parent_path());
+	fs::path multiPath = parentPath.parent_path() / "assets" / "sub" / "subsub";
+	//macht verzeichnis+ evtl fehlende zwischenverzeichnisse
+	if (!fs::exists(multiPath))
+	{
+		fs::create_directories(multiPath);
+	}
+	ASSERT_TRUE(fs::exists(multiPath));
+	fs::remove_all(parentPath.parent_path() / "assets" / "sub");
+	ASSERT_TRUE(fs::exists(parentPath.parent_path() / "assets"));
+	ASSERT_TRUE(!fs::exists(parentPath.parent_path() / "assets" / "sub" ));
+}
